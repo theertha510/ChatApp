0f8f75f0b603a029f9ff5be1e27a35a12f868f0e
theertha510 <theertha@bititude.com>
2023-11-23 10:58:35 +0530

    text changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ function Home() {
     <div className='flex items-center'>
       <div className='bg-grey1 p-10 w-[500px] h-[500px] rounded-3xl flex flex-col justify-between'>
         <div>
-          <h1 className='text-white text-center'>Start Chat</h1>
+          <h1 className='text-white text-center text-2xl'>Start Chat</h1>
         </div>
         <div className='flex'>
           <input placeholder='Message...' className='p-2 rounded-md w-full' />
